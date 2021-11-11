96ae6207d1a8efc9aa9d1fa49d8b9a7a00a6f68a
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-11-11 00:08:20 -0500

    updated readme.md

--- src/App.js
+++ src/App.js
@@ -3,27 +3,13 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
-import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 import Footer from './components/Footer';
-
 import {Container} from "react-bootstrap";
 
 
-
 function App() {
-  // const [categories] = useState([
-  //   {
-  //     name: 'portfolio',
-  //     description: 'Web development Portfolio',
-      
-  //   },
-  //   {
-  //     name: 'resume',
-  //     description: 'My Resume',
-      
-  //   },
-  // ]);
+
 
   // const [currentCategory, setCurrentCategory] = useState(categories[0]);
   const [contactSelected, setContactSelected] = useState(false);
@@ -82,46 +68,6 @@ function App() {
       </Container>
       </div>
 
-    // <div>
-
-    // <header></header>
-    //   <Nav
-    //     categories={categories}
-    //     setCurrentCategory={setCurrentCategory}
-    //     currentCategory={currentCategory}
-    //     contactSelected={contactSelected}
-    //     setContactSelected={setContactSelected}
-    //     portfolioSelected={portfolioSelected}
-    //     setPortfolioSelected={setPortfolioSelected}
-    //     resumeSelected={resumeSelected}
-    //     setResumeSelected={setResumeSelected}
-
-    //   ></Nav>
-    //   <main>
-    //     {!contactSelected ? (
-    //       <>
-    //         <Gallery currentCategory={currentCategory}></Gallery>
-    //         <About></About>
-    //         <Resume></Resume>
-    //       </>
-    //     ) : (
-    //         <ContactForm></ContactForm>
-            
-    //       )}
-    //       {!portfolioSelected ? (
-    //         <>
-  
-    //         <Portfolio></Portfolio>
-    //         </>
-    //       ) : (
-    //         <Resume></Resume>
-              
-    //         )}
-       
-          
-    //   </main>
-    //   <Footer></Footer>
-    // </div>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,43 +1,39 @@
 import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import {Navbar, Nav, Container} from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function customNav(props) {
-    const {
-      contactSelected,
-      setContactSelected,
-      aboutSelected,
-      setAboutSelected,
-      portfolioSelected,
-      setPortfolioSelected,
-      resumeSelected,
-      setResumeSelected
+  const {
+    contactSelected,
+    setContactSelected,
+    aboutSelected,
+    setAboutSelected,
+    portfolioSelected,
+    setPortfolioSelected,
+    resumeSelected,
+    setResumeSelected
   } = props;
 
-  function handleClick(target){
+  function handleClick(target) {
     setContactSelected(false);
     setPortfolioSelected(false);
     setAboutSelected(false);
     setResumeSelected(false);
 
-    if (target===0){
+    if (target === 0) {
       setContactSelected(true);
     }
-    if (target===1){
+    if (target === 1) {
       setAboutSelected(true);
     }
-    if(target===2){
+    if (target === 2) {
       setPortfolioSelected(true);
     }
-    if(target===3){
+    if (target === 3) {
       setResumeSelected(true);
     }
   }
 
-
-  // useEffect(() => {
-  //   document.title = capitalizeFirstLetter(currentCategory.name);
-  // }, [currentCategory]);
 
   return (
     <header>
@@ -48,28 +44,28 @@ function customNav(props) {
             <Nav className="me-auto">
               {contactSelected ? (
                 <>
-                <Nav.Link id="nav-selected">Contact</Nav.Link>
+                  <Nav.Link id="nav-selected">Contact</Nav.Link>
                 </>
               ) : (
                 <><Nav.Link onClick={() => handleClick(0)}> Contact </Nav.Link></>
               )}
               {aboutSelected ? (
                 <>
-                <Nav.Link id="nav-selected">About Me</Nav.Link>
+                  <Nav.Link id="nav-selected">About Me</Nav.Link>
                 </>
               ) : (
                 <><Nav.Link onClick={() => handleClick(1)}> About Me </Nav.Link></>
               )}
               {portfolioSelected ? (
                 <>
-                <Nav.Link id="nav-selected">Portfolio</Nav.Link>
+                  <Nav.Link id="nav-selected">Portfolio</Nav.Link>
                 </>
               ) : (
                 <><Nav.Link onClick={() => handleClick(2)}> Portfolio </Nav.Link></>
               )}
               {resumeSelected ? (
                 <>
-                <Nav.Link id="nav-selected">Resume</Nav.Link>
+                  <Nav.Link id="nav-selected">Resume</Nav.Link>
                 </>
               ) : (
                 <><Nav.Link onClick={() => handleClick(3)}> Resume </Nav.Link></>
@@ -79,46 +75,11 @@ function customNav(props) {
         </Container>
       </Navbar>
     </header>
-  )}
+  )
+}
 
-  // return (
-  //   <header className="flex-row px-1">
-  //     <h2>
-  //       <a data-testid="link" href="/">
-  //         <span role="img" aria-label="camera"> 🌎📸</span> Colin Nebula
-  //       </a>
-  //     </h2>
-  //     <nav>
-  //       <ul className="flex-row">
-  //         <li className="mx-2">
-  //         <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-  //             About me
-  //           </a>
-  //         </li>
-  //         <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-  //           <span onClick={() => setContactSelected(true)}>Contact</span>
-  //         </li>
-  //         {categories.map((category) => (
-  //           <li
-  //             className={`mx-1 ${
-  //               currentCategory.name === category.name && !contactSelected && 'navActive'
-  //               }`}
-  //             key={category.name}
-  //           >
-  //             <span
-  //               onClick={() => {
-  //                   setCurrentCategory(category);
-  //                   setContactSelected(false);
-  //               }}
-  //             >
-  //               {capitalizeFirstLetter(category.name)}
-  //             </span>
-  //           </li>
-  //         ))}
-  //       </ul>
-  //     </nav>
-  //   </header>
-  
+
+
 
 
 export default customNav;
