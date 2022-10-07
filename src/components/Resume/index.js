b1a97532dca77584e4db7e10c6ce44545cf490c5
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-10-06 22:35:26 -0400

    updated the resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import myResume from '../../assets/files/resume.PDF';
+import {  
+	FaGithub, 
+	FaHtml5, 
+	FaCss3Alt, 
+	FaJsSquare, 
+	FaBootstrap, 
+	FaNode, FaReact, 
+	FaNpm, 
+	FaCloudDownloadAlt 
+} 
+from "react-icons/fa";
 
 
 function Resume() {
@@ -14,13 +26,18 @@ function Resume() {
                 </h2>
                 <h4>Full Stack Developer</h4>
                 <p>
-                   HTML | CSS | JavaScript | AJAX | Bootstrap | Rest | MYSQL | NodeJs | NoSQL | React | MERN | 
-                   MongoDB | SQL | JQuery | Git | Responsive Design | DOM | Progressive Web Applications (PWA) | 
-                   Node.js | Express.js | Object Relational Mapping(ORM) | React | Redux | Model-View-Controller (MVC) | IndexedDB
+                   <FaHtml5/>HTML | <FaCss3Alt/>CSS | <FaJsSquare/>JavaScript | AJAX | <FaBootstrap/>Bootstrap | Rest | MYSQL | NodeJs | NoSQL | React | MERN | 
+                   MongoDB | SQL | JQuery | <FaGithub/>Git | Responsive Design | DOM | Progressive Web Applications (PWA) | 
+                   <FaNode/>Node.js | Express.js | Object Relational Mapping(ORM) | <FaReact/>React | Redux | Model-View-Controller (MVC) | IndexedDB | <FaNpm/>NPM
 
                 </p>
+
+                <a href={myResume}download>
+                    <h4>Resume Download<FaCloudDownloadAlt/></h4>
+                </a>
                 
             </div>
+            
         </section>
     )
 }
