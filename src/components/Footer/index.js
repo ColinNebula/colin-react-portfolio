74d8e2deab69e6b6fc65404984695b67af239cfc
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-10-13 00:50:23 -0400

    updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -23,7 +23,7 @@ function Footer() {
                     
                         
                         <div>
-                        <a href="/home"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp;Home</a>
+                        <a href="/"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp;Home</a>
                         <br/>
                         &copy; 2022 colinnebula@hotmail.com.
                         </div>
