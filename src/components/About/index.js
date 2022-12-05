d570a8de81515e25399fed07a9ab4c0ac1eaa278
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-12-05 11:33:25 -0500

    fixed layout issue

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,39 +2,39 @@ import React from 'react';
 import cNebula from "../../assets/images/c-nebula.jpg";
 import { Button, Card, Container, Col, Row, Image } from 'react-bootstrap';
 
-function About(props) {
+function About() {
   return (
     <Container fluid>
       <Row>
-        <Col md={{ span: 6, offset: 3 }}>
+        <Col md={{ span: 6, offset: 3 }} className="mb-3">
+        
           <Card className="bg-dark text-white" style={{ width: '30rem' }}>
+        
             <Card.Img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.UqmbTVnNzHJt0VRJ2FQwPwHaD5%26pid%3DApi&f=1&ipt=1e636f784b2a86208c855ce17853192dd34c2464047f8803b8b4ba0aa6e5469f&ipo=images/100px270" className="rounded" alt="Card image" />
             <Card.ImgOverlay>
               <Card.Title className="text-white"> Full Stack Web Developer</Card.Title>
-              <h1>
+              <Card.Text><h1>
                 Hi, my name is
                 Colin Nebula
               </h1>
-            </Card.ImgOverlay>
+              </Card.Text>
+          </Card.ImgOverlay>
           </Card>
-
-          <Card className="bg-dark text-light shadow-lg rounded" style={{ width: '30rem' }}>
-            <Card.Img variant="top" className="rounded" src={cNebula} fluid alt="C Nebula" />
-            <Card.Body>
-              <Card.Header>Full Stack Developer</Card.Header>
+          
+          <Card className="bg-dark text-light shadow-lg" style={{ width: '30rem' }}>
+          <Card.Img variant="top" className="rounded" src={cNebula} fluid alt="C Nebula" />
+          <br />
+          
+          <Card.Header>Full Stack Developer</Card.Header>
               <Card.Text>
                 As a self starter who loves being part of a team as well, I bring discipline and
                 rigour from my years serving the US Military. This, in addition to my numerous diplomas
                 in digital art, technology, animation, and now coding, have helped me reach my present
                 position where I hope to develop websites.
               </Card.Text>
-            </Card.Body>
             <Card.Footer>
-              <Button variant="outline-danger" href="/portfolio">Go to portfolio
-
-              </Button>
+              <Button variant="outline-danger" href="/portfolio">Go to portfolio</Button>
             </Card.Footer>
-
           </Card>
         </Col>
       </Row>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -11,97 +11,98 @@ import {
     FaCloudDownloadAlt
 }
     from "react-icons/fa";
-import {Button, Card, Container, Row, Col } from 'react-bootstrap';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 
 
 function Resume() {
 
     return (
         <Container fluid>
-          <Row>
-            <Col ms={"auto"}>
-            
-            <Card className="bg-dark text-white shadow-lg" style={{ color: "#000", width: "auto" }}>
-        
-            <h1 id="resume">Web Development Resume</h1>
+            <Row>
+                <Col ms={"auto"}>
 
-            
-                <Card.Header>
-                    Colin Nebula
-
-                </Card.Header>
-                <Card.Title>Full Stack Developer</Card.Title>
-                <br />
-                <Card.Text>
-                    To become industry standard, we learned to utilize a variety of industry tools to add to our skill set.
-                    Some of these tools include:
-
-                   </Card.Text>
-                    <br />
-                    <Card.Text>
-                    <FaHtml5 />HTML | <FaCss3Alt />CSS | <FaJsSquare />JavaScript | AJAX | <FaBootstrap />Bootstrap | Rest  | MYSQL | NoSQL | <FaReact />React | MERN |
-                    MongoDB | SQL | JQuery | <FaGithub />Git | Responsive Design | DOM | Progressive Web Applications (PWA) |
-                    <FaNode />Node.js | Express.js | Object Relational Mapping(ORM) | <FaReact />React Font end Framework | Redux | Model View Controller(MVC) | IndexedDB | <FaNpm />NPM
-                    </Card.Text>
-                    
-                    
-                    </Card>
-    
                     <Card className="bg-dark text-white shadow-lg" style={{ color: "#000", width: "auto" }}>
-                    <Card.Header>Recent Projects</Card.Header>
-                    <Card.Body>
-                    <br />
-                    <Card.Title>Enviro-Dashboard</Card.Title>
-                    <Card.Text>
-                    Working and collaborating with peers online through Zoom.
-                    Conceptualizing the project together as a team.
-                    Create an MVP within limited time constraints to meet deadlines.
-                    Utilizing the GitHub workflow.
-                    </Card.Text>
-    
-                    <br />
-                    <p>
-                    <h4> Tools and technologies used:</h4>
-                    <FaHtml5 />HTML, | Bulma, <FaCss3Alt />CSS, | <FaJsSquare />JavaScript, | <FaNode />Node.js, | MySQL, | Handlebars, | Express, | Bcrypt, | Dotenv
-                    </p>
-                    <br />
-                    </Card.Body>
-                    <Card.Footer className="text-muted">2 days ago
-                    <Button variant="outline-danger" href={"https://theteam33333.github.io/enviro-dashboard/"}>
-                        <h6>Enviro-Dashboard</h6>
-                    </Button>
-                        
-                    <Button variant="outline-danger" href="https://github.com/theteam33333/enviro-dashboard/"><i class="<FaGithub/>" aria-hidden="true"></i>&nbsp;GitHub
-                    </Button>
-                    <FaGithub />
-                </Card.Footer>
-                </Card>
 
-            <Card className="bg-dark text-white shadow-lg" style={{ color: "#000", width: "auto" }}>
-                <Card.Header>The Squid Store</Card.Header>
-                <Card.Text>
-                Another Team collaboration with peers online through Zoom.
-                Conceptualizing the project together as a team.
-                Create a web application while utilizing GitHub to save different versions of the project.
-                </Card.Text>
-                <br />
-                <Card.Text>
-                <h4> Tools and technologies used:</h4>
-                <FaHtml5 />HTML, | <FaCss3Alt />CSS, | MERN Stack E-commerce, | JWX, <FaJsSquare />JavaScript, | <FaNode />Nodejs, | MongoDB, | Express, | Bcrypt, | GraphQL, | Stripe, | <FaNpm />NPM
-                </Card.Text>    
-                <Card.Footer className="text-muted">2 days ago
-                <a href={'https://thesquidstore.herokuapp.com/'}>
-                    <h6>The Squid Store</h6>
-                    <Button variant="outline-danger" href="https://github.com/TPhondeth/the-squid-store"><i className="<FaGithub/>" aria-hidden="true"></i>&nbsp;GitHub</Button>
-                    <FaGithub />
-                </a>
-                    <Button variant="outline-danger" href={myResume}>{myResume.name} <FaCloudDownloadAlt /> Resume</Button>
-                    <a href={myResume} download>
-                    </a>
-                </Card.Footer>
-        </Card>
-        </Col>
-        </Row>
+                        <h1 id="resume">Web Development Resume</h1>
+
+
+                        <Card.Header>
+                            Colin Nebula
+
+                        </Card.Header>
+                        <Card.Body>
+                        <Card.Title>Full Stack Developer</Card.Title>
+                        <br />
+                        <Card.Text>
+                            To become industry standard, we learned to utilize a variety of industry tools to add to our skill set.
+                            Some of these tools include:
+
+                        </Card.Text>
+                        <br />
+                        <Card.Text>
+                            <FaHtml5 />HTML | <FaCss3Alt />CSS | <FaJsSquare />JavaScript | AJAX | <FaBootstrap />Bootstrap | Rest  | MYSQL | NoSQL | <FaReact />React | MERN |
+                            MongoDB | SQL | JQuery | <FaGithub />Git | Responsive Design | DOM | Progressive Web Applications (PWA) |
+                            <FaNode />Node.js | Express.js | Object Relational Mapping(ORM) | <FaReact />React Font end Framework | Redux | Model View Controller(MVC) | IndexedDB | <FaNpm />NPM
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                
+
+                    <Card className="bg-dark text-white shadow-lg" style={{ color: "#000", width: "auto" }}>
+                        <Card.Header>Recent Projects</Card.Header>
+                        <Card.Body>
+                            <br />
+                            <Card.Title>Enviro-Dashboard</Card.Title>
+                            <Card.Text>
+                                Working and collaborating with peers online through Zoom.
+                                Conceptualizing the project together as a team.
+                                Create an MVP within limited time constraints to meet deadlines.
+                                Utilizing the GitHub workflow.
+                            </Card.Text>
+
+                            <br />
+                            <Card.Text>
+                                <Card.Title> Tools and technologies used:</Card.Title>
+                                <FaHtml5 />HTML, | Bulma, <FaCss3Alt />CSS, | <FaJsSquare />JavaScript, | <FaNode />Node.js, | MySQL, | Handlebars, | Express, | Bcrypt, | Dotenv
+                            </Card.Text>
+                            <br />
+                        </Card.Body>
+                        <Card.Footer className="text-muted">
+                            <Button variant="outline-danger" href={"https://theteam33333.github.io/enviro-dashboard/"}>
+                                <h6>Enviro-Dashboard</h6>
+                            </Button>
+
+                            <Button variant="outline-danger" href="https://github.com/theteam33333/enviro-dashboard/"><i class="<FaGithub/>" aria-hidden="true"></i>&nbsp;GitHub
+                            </Button>
+                            
+                        </Card.Footer>
+                    </Card>
+
+                    <Card className="bg-dark text-white shadow-lg" style={{ color: "#000", width: "auto" }}>
+                        <Card.Header>The Squid Store</Card.Header>
+                        <Card.Text>
+                            Another Team collaboration with peers online through Zoom.
+                            Conceptualizing the project together as a team.
+                            Create a web application while utilizing GitHub to save different versions of the project.
+                        </Card.Text>
+                        <br />
+                        <Card.Text>
+                            <Card.Title> Tools and technologies used:</Card.Title>
+                            <FaHtml5 />HTML, | <FaCss3Alt />CSS, | MERN Stack E-commerce, | JWX, <FaJsSquare />JavaScript, | <FaNode />Nodejs, | MongoDB, | Express, | Bcrypt, | GraphQL, | Stripe, | <FaNpm />NPM
+                        </Card.Text>
+                        <Card.Footer className="text-muted">2 days ago
+                            <a href={'https://thesquidstore.herokuapp.com/'}>
+                                <h6>The Squid Store</h6>
+                                <Button variant="outline-danger" href="https://github.com/TPhondeth/the-squid-store"><i className="<FaGithub/>" aria-hidden="true"></i>&nbsp;GitHub</Button>
+                                <FaGithub />
+                            </a>
+                            <Button variant="outline-danger" href={myResume}>{myResume.name} <FaCloudDownloadAlt /> Resume</Button>
+                            <a href={myResume} download>
+                            </a>
+                        </Card.Footer>
+                    </Card>
+                </Col>
+            </Row>
         </Container>
     )
 }
