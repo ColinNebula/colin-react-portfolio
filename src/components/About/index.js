2949cd29e3b29f34b2151df62cdea9e2590237cd
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2022-12-05 13:46:11 -0500

    updated about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
       <Row>
         <Col md={{ span: 6, offset: 3 }} className="mb-3">
         
-          <Card className="bg-dark text-white" style={{ width: '30rem' }}>
+          <Card className="bg-dark text-white" style={{ width: '24rem' }}>
         
             <Card.Img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.UqmbTVnNzHJt0VRJ2FQwPwHaD5%26pid%3DApi&f=1&ipt=1e636f784b2a86208c855ce17853192dd34c2464047f8803b8b4ba0aa6e5469f&ipo=images/100px270" className="rounded" alt="Card image" />
             <Card.ImgOverlay>
@@ -21,8 +21,8 @@ function About() {
           </Card.ImgOverlay>
           </Card>
           
-          <Card className="bg-dark text-light shadow-lg" style={{ width: '30rem' }}>
-          <Card.Img variant="top" className="rounded" src={cNebula} fluid alt="C Nebula" />
+          <Card className="bg-dark text-light shadow-lg" style={{ width: '24rem' }}>
+          <Card.Img variant="top" className="rounded" src={cNebula} alt="C Nebula" />
           <br />
           
           <Card.Header>Full Stack Developer</Card.Header>
@@ -33,7 +33,7 @@ function About() {
                 position where I hope to develop websites.
               </Card.Text>
             <Card.Footer>
-              <Button variant="outline-danger" href="/portfolio">Go to portfolio</Button>
+              <Button variant="outline-danger" href={"portfolio"}>Go to portfolio</Button>
             </Card.Footer>
           </Card>
         </Col>
